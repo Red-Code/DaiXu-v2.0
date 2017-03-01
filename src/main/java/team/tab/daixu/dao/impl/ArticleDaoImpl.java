04210d88e066609562140ea124e47760b8dc97d2
Red-Code <18095953699@163.com>
2017-03-01 21:22:21 +0800

    created a association query demo

--- src/main/java/team/tab/daixu/dao/impl/ArticleDaoImpl.java
+++ src/main/java/team/tab/daixu/dao/impl/ArticleDaoImpl.java
@@ -1,36 +1,37 @@
 package team.tab.daixu.dao.impl;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import org.hibernate.query.Query;
 import team.tab.daixu.dao.ArticleDao;
 import team.tab.daixu.entity.ArticleEntity;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by CLY on 2017/1/5.
  */
 public class ArticleDaoImpl implements ArticleDao {
-    @Override
-    public void save(ArticleEntity articleEntity) {
-
-    }
+    @Resource(name = "sessionFactory")
+    private SessionFactory sessionFactory;//注入sessionFactory
 
     @Override
-    public void delete(int id) {
-
-    }
-
-    @Override
-    public void update(ArticleEntity articleEntity) {
-
-    }
-
-    @Override
-    public ArticleEntity findById(int id) {
+    public ArticleEntity act_save(ArticleEntity articleEntity) {
         return null;
     }
 
     @Override
-    public List<ArticleEntity> findAll() {
+    public ArticleEntity findOneById(Integer article_id) {
+        String hql = "from ArticleEntity where articleId="+article_id;//hql语句
+        Query query = sessionFactory.getCurrentSession().createQuery(hql);
+
+        List<ArticleEntity> list_article = query.list();
+        System.out.print("list结构"+list_article.get(0).getArticleName());
+
         return null;
     }
 }
